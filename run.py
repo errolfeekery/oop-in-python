81596bd247768a91667b7f52b68ff48a373b6297
errolfeekery <errolfeekery@gmail.com>
2022-12-28 16:30:11 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Item:
+    def calculate_total_price(self, x, y):
+        return x * y
+
+
+item1 = Item()
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5
+print(item1.calculate_total_price(item1.price, item1.quantity))
+
+item2 = Item()
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 3
+
