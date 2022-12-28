2b590f7db025a92e82723263cb6cea375651848a
errolfeekery <errolfeekery@gmail.com>
2022-12-28 16:53:06 +0000

    add assert

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 class Item:
-    def __init__(self, name, price, quantity):
+    def __init__(self, name: str, price: float, quantity=0):      
+        assert price >= 0, f"Price {price} is not greater or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+
         self.name = name
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self, x, y):
-        return x * y
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
 item1 = Item("Phone", 100, 5)
