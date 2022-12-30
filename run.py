4c41673e7b8e30ec6c33c86cc42f6d18a5c2923c
errolfeekery <errolfeekery@gmail.com>
2022-12-30 02:14:09 +0000

    add api's

--- run.py
+++ run.py
@@ -1,5 +1,27 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+
+print(data)
+
+
 class Item:
     pay_rate = 0.8
+    all = []
 
     def __init__(self, name: str, price: float, quantity=0):
         assert price >= 0, \
@@ -11,16 +33,22 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price = self.price * Item.pay_rate
 
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-item1 = Item("Phone", 100, 5)
 
-item2 = Item("Laptop", 1000, 3)
+item1 = Item("Phone", 100, 3)
+item2 = Item("Laptop", 1000, 4)
+item3 = Item("Cable", 10, 15)
+item4 = Item("Mouse", 50, 100)
+item5 = Item("Keyboard", 75, 10)
 
-print(Item.__dict__)
-print(item1.__dict__)
+print(Item.all)
